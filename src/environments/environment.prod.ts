9301abf1d1a4286a550373ec69bdd12da89c726a
jerry-ebikari <jerryebikarineedam@gmail.com>
2022-10-15 11:08:00 +0100

    Added firebase to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'skill-assessment-quiz',
+    appId: '1:619137423330:web:d79bdb2994c650abc60bbb',
+    storageBucket: 'skill-assessment-quiz.appspot.com',
+    apiKey: 'AIzaSyCZk3h-adKebEY7tAWlrrUd3HaS7dwSuis',
+    authDomain: 'skill-assessment-quiz.firebaseapp.com',
+    messagingSenderId: '619137423330',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'skill-assessment-quiz',
+    appId: '1:619137423330:web:d79bdb2994c650abc60bbb',
+    storageBucket: 'skill-assessment-quiz.appspot.com',
+    apiKey: 'AIzaSyCZk3h-adKebEY7tAWlrrUd3HaS7dwSuis',
+    authDomain: 'skill-assessment-quiz.firebaseapp.com',
+    messagingSenderId: '619137423330',
+  },
   production: false
 };
 
