6b8b33b4a59e031162f4b0c2fe18a651857b6b6a
jerry-ebikari <jerryebikarineedam@gmail.com>
2022-10-18 14:59:12 +0100

    Implemented login and signup pages designs

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form!: FormGroup;
+  passwordVisible: boolean = false;
+  confirmPasswordVisible: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl("", [Validators.email, Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    })
   }
 
+  togglePasswordVisibility(passwordField?: any) {
+    if (passwordField!.name == "password") {
+      this.passwordVisible =  !this.passwordVisible;
+    } else {
+      this.confirmPasswordVisible = !this.confirmPasswordVisible;
+    }
+  }
 }
--- src/app/core/components/register/register.component.ts
+++ src/app/core/components/register/register.component.ts
@@ -8,18 +8,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
-  isPasswordVisible: boolean = false;
+  passwordVisible: boolean = false;
+  confirmPasswordVisible: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl("", [Validators.email, Validators.required]),
-      password: new FormControl("", [Validators.required])
+      confirmPassword: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
     })
   }
 
-  togglePasswordVisibility() {
-    this.isPasswordVisible = !this.isPasswordVisible;
+  togglePasswordVisibility(passwordField?: any) {
+    if (passwordField!.name == "password") {
+      this.passwordVisible =  !this.passwordVisible;
+    } else {
+      this.confirmPasswordVisible = !this.confirmPasswordVisible;
+    }
   }
 
 }
